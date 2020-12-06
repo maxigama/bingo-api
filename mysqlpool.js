c5fb3e209d713007ef5bc94aef997abf05ceb580
Maximiliano Leonel Gamarra <maxi_gama@hotmail.com>
2020-12-05 22:10:30 -0300

    Cnn

--- mysqlpool.js
+++ mysqlpool.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql'); 
 const config = {
        
-      host: "localhost",
-        user: "root",
-        password: "328ELMsTZtqQ",
-        database: "BingoApiRest"
+      host: "us-cdbr-east-02.cleardb.com",
+        user: "b9a9761fa59248",
+        password: "f971e9e2",
+        database: "heroku_1771ca38fd136df"
   };
  
 // promise style:
@@ -19,3 +19,5 @@ con.connect(function(err) {
 module.exports = {
     mysql, con
 }
+
+//mysql://b9a9761fa59248:f971e9e2@us-cdbr-east-02.cleardb.com/heroku_1771ca38fd136df?reconnect=true
